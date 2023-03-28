73cbed44a8c9eb850b4c0bd6f950f0dfdd7024b4
Tyler Eckhardt <111406810+eckhatyl000@users.noreply.github.com>
2023-03-28 10:46:16 -0600

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,1 @@
+let color = button.color 
--- tyler.about.js
+++ tyler.about.js
@@ -1,32 +1,31 @@
-var form = document.querySelector('form');
-
-    form.addEventListener('submit', (event) => {
-	event.preventDefault();
-	alert('Form submitted successfully!');
-});
-
-
-
-
 function handleSubmit(evt) {
 	evt.preventDefault();
-	
 	console.log('form submit');
 }
 
-
 let form = document.querySelector('#contact');
-
 form.addEventListener('submit', handleSubmit);
-const image = document.querySelector('img');
 
+const image = document.querySelector('img');
 const compliments = [
 	'You are committed to the cause',
-    'You are so cool',
-    'You understand the role that the great God AMWAY plays in our lives and are dedicated to selling for them'
+	'You are so cool',
+	'You understand the role that the great God AMWAY plays in our lives and are dedicated to selling for them'
 ];
 
 image.addEventListener('mouseover', () => {
 	const randomCompliment = compliments[Math.floor(Math.random() * compliments.length)];
 	alert(randomCompliment);
 });
+
+const toggleButton = document.getElementById('toggleButton');
+const toggleParagraph = document.getElementById('toggleParagraph');
+
+toggleButton.addEventListener('click', function () {
+	if (toggleParagraph.style.display === 'none') {
+		toggleParagraph.style.display = 'block';
+	} else {
+		toggleParagraph.style.display = 'none';
+	}
+});
+
